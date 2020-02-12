f253d8c6a97277c32186dbb63e6fa883567591dc
Student <example@example.com>
2020-02-11 20:46:55 -0800

    Create a webpage that shows the images of cat breeds when user clicks on breed names

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#siberian").click(function() {
+		// var siberian = document.createElement("siberian-cat");
+		// siberian.setAttribute("src, "siberian-cat.jpg");
+		$(this).before('<img src="img/siberian-cat.jpg" />');
+		
+	});
+});
+
